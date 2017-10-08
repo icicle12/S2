526f4197dab3bd768c346410bcd38b8b41710e81
icicle12 <mistcomme@gmail.com>
2017-10-08 01:40:26 -0700

    Update Resume.js

--- S2/js/Resume.js
+++ S2/js/Resume.js
@@ -1,43 +1,88 @@
-//Some code taken from 'Bookmarks!' Script
+//// Some code taken from '.bookmarks'
 registerPlugin({
     name: 'Resume',
     version: '1.0',
+    backends: ['ts3', 'discord'],
+    engine: '>= 0.9.16',
     description: 'Enter !pause to pause the song, enter !resume to seek to unpause the song.',
-    author: 'Shawye <shawye@ucla.ed>','Bert'
+    author: 'Shawye <shawye@ucla.edu>',
     vars: []
 }, function (sinusbot, config) {
 
-    var store = require('store'),
-        media = require('media'),
-        backend = require('backend'),
-        audio = require('audio');
+    //    var store = require('store'),
+    //        media = require('media'),
+    //        backend = require('backend'),
+    //        audio = require('audio');
 
-    var event = require('event');
-    event.on('chat', function (ev) {
-        ev.client.chat('Hi, ' + ev.client.name() + ', you just wrote: ' + ev.text);
-    });
+    //    var event = require('event');
+    //    event.on('chat', function (ev) {
+    //        ev.client.chat('Hi, ' + ev.client.name() + ', you just wrote: ' + ev.text);
+    //    });
 
-    event.on('chat', function (ev) {
-        engine.log(ev.text);
-        if (ev.text == '.bookmark') {
-            engine.log("bookmark");
-            var track = media.getCurrentTrack();
+    //    event.on('chat', function (ev) {
+    //        engine.log(ev.text);
+    //        if (ev.text == '.bookmark') {
+    //            engine.log("bookmark");
+    //            var track = media.getCurrentTrack();
+    //            if (!track) return;
+    //            var pos = audio.getTrackPosition();
+    //            store.set(track.ID(), pos);
+    //            backend.getCurrentChannel().chat('Position saved for track ' + track.uuid + ' at ' + pos + 'ms.');
+    //            media.stop();
+    //        }
+
+    //        if (ev.text == '!resume') {
+    //            engine.log("resume");
+    //            var track = media.getCurrentTrack();
+    //            if (!track) return;
+    //            var pos = store.get(track.ID());
+    //            if (!pos) {
+    //                backend.getCurrentChannel().chat('No position found, sorry.');
+    //                return;
+    //            }
+    //            track.play();
+    //            audio.seek(pos);
+    //            backend.getCurrentChannel().chat('Resumed at ' + pos + 'ms.');
+    //        }
+    //    });
+    //});
+
+    //registerPlugin({
+    //    name: 'Bookmarks!',
+    //    version: '1.0',
+    //    description: 'Enter .bookmark to save the current position, enter .resume to seek to the bookmarked position.',
+    //    author: 'Michael Friese <michael@sinusbot.com>',
+    //    vars: {}
+    //}, function (sinusbot, config) {
+    sinusbot.on('chat', function (ev) {
+        if ((ev.msg == '!pause') && (ev.mode == 2)) {
+            var track = sinusbot.getCurrentTrack();
             if (!track) return;
-            var pos = audio.getTrackPosition();
-            store.set(track.ID(), pos);
-            backend.getCurrentChannel().chat('Position saved for track ' + track.uuid + ' at ' + pos + 'ms.');
+            var pos = sinusbot.getPos();
+            sinusbot.setVar(track.uuid, pos);
+            sinusbot.chatChannel('Position saved for track ' + track.uuid + ' at ' + pos + 'ms.');
+            sinusbot.stop();
         }
-        if (ev.text == '.resume') {
-            engine.log("resume");
-            var track = media.getCurrentTrack();
+
+        if ((ev.msg == '!resume') && (ev.mode == 2)) {
+            var track = sinusbot.getCurrentTrack();
             if (!track) return;
-            var pos = store.get(track.ID());
+            var pos = sinusbot.getVar(track.uuid);
             if (!pos) {
-                backend.getCurrentChannel().chat('No position found, sorry.');
+                sinusbot.chatChannel('No position found, sorry.');
                 return;
             }
-            audio.seek(pos);
-            backend.getCurrentChannel().chat('Resumed at ' + pos + 'ms.');
+            sinusbot.setMute(true);
+            sinusbot.play();
+            sinusbot.setMute(true);
+            setTimeout(function () {
+            sinusbot.seek(pos);
+            sinusbot.chatChannel('Resumed at ' + pos + 'ms.');
+            }, 50);
+            setTimeout(function () {
+            sinusbot.setMute(false);
+            }, 500);
+            
         }
     });
 });
