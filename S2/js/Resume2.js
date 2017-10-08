4a3ca8d73f0897e300e38bc33dd292c83cf0a320
icicle12 <mistcomme@gmail.com>
2017-10-07 23:32:08 -0700

    Update Resume2.js

--- S2/js/Resume2.js
+++ S2/js/Resume2.js
@@ -5,55 +5,55 @@ registerPlugin({
     backends: ['ts3', 'discord'],
     engine: '>= 0.9.16',
     description: 'Enter !pause to pause the song, enter !resume to seek to unpause the song.',
-    author: 'Shawye <shawye@ucla.ed>',
+    author: 'Shawye <shawye@ucla.edu>',
     vars: []
 }, function (sinusbot, config) {
 
-//    var store = require('store'),
-//        media = require('media'),
-//        backend = require('backend'),
-//        audio = require('audio');
+    //    var store = require('store'),
+    //        media = require('media'),
+    //        backend = require('backend'),
+    //        audio = require('audio');
 
-//    var event = require('event');
-//    event.on('chat', function (ev) {
-//        ev.client.chat('Hi, ' + ev.client.name() + ', you just wrote: ' + ev.text);
-//    });
+    //    var event = require('event');
+    //    event.on('chat', function (ev) {
+    //        ev.client.chat('Hi, ' + ev.client.name() + ', you just wrote: ' + ev.text);
+    //    });
 
-//    event.on('chat', function (ev) {
-//        engine.log(ev.text);
-//        if (ev.text == '.bookmark') {
-//            engine.log("bookmark");
-//            var track = media.getCurrentTrack();
-//            if (!track) return;
-//            var pos = audio.getTrackPosition();
-//            store.set(track.ID(), pos);
-//            backend.getCurrentChannel().chat('Position saved for track ' + track.uuid + ' at ' + pos + 'ms.');
-//            media.stop();
-//        }
-        
-//        if (ev.text == '!resume') {
-//            engine.log("resume");
-//            var track = media.getCurrentTrack();
-//            if (!track) return;
-//            var pos = store.get(track.ID());
-//            if (!pos) {
-//                backend.getCurrentChannel().chat('No position found, sorry.');
-//                return;
-//            }
-//            track.play();
-//            audio.seek(pos);
-//            backend.getCurrentChannel().chat('Resumed at ' + pos + 'ms.');
-//        }
-//    });
-//});
+    //    event.on('chat', function (ev) {
+    //        engine.log(ev.text);
+    //        if (ev.text == '.bookmark') {
+    //            engine.log("bookmark");
+    //            var track = media.getCurrentTrack();
+    //            if (!track) return;
+    //            var pos = audio.getTrackPosition();
+    //            store.set(track.ID(), pos);
+    //            backend.getCurrentChannel().chat('Position saved for track ' + track.uuid + ' at ' + pos + 'ms.');
+    //            media.stop();
+    //        }
 
-//registerPlugin({
-//    name: 'Bookmarks!',
-//    version: '1.0',
-//    description: 'Enter .bookmark to save the current position, enter .resume to seek to the bookmarked position.',
-//    author: 'Michael Friese <michael@sinusbot.com>',
-//    vars: {}
-//}, function (sinusbot, config) {
+    //        if (ev.text == '!resume') {
+    //            engine.log("resume");
+    //            var track = media.getCurrentTrack();
+    //            if (!track) return;
+    //            var pos = store.get(track.ID());
+    //            if (!pos) {
+    //                backend.getCurrentChannel().chat('No position found, sorry.');
+    //                return;
+    //            }
+    //            track.play();
+    //            audio.seek(pos);
+    //            backend.getCurrentChannel().chat('Resumed at ' + pos + 'ms.');
+    //        }
+    //    });
+    //});
+
+    //registerPlugin({
+    //    name: 'Bookmarks!',
+    //    version: '1.0',
+    //    description: 'Enter .bookmark to save the current position, enter .resume to seek to the bookmarked position.',
+    //    author: 'Michael Friese <michael@sinusbot.com>',
+    //    vars: {}
+    //}, function (sinusbot, config) {
     sinusbot.on('chat', function (ev) {
         if (ev.msg == '!pause') {
             var track = sinusbot.getCurrentTrack();
@@ -61,32 +61,28 @@ registerPlugin({
             var pos = sinusbot.getPos();
             sinusbot.setVar(track.uuid, pos);
             sinusbot.chatChannel('Position saved for track ' + track.uuid + ' at ' + pos + 'ms.');
-            sinusbot.stop();
+            //sinusbot.setMute(true);
+            sinusbot.setRepeat(true);
+        }
+        var intervalID = window.setInterval(myCallback, 30000)
+
+        function myCallback() {
+            sinusbot.seek(100);
         }
 
-        if (ev.text == '!resume') {
-            sinusbot.getCurrentChannel().chat('test 2');
+        if (ev.msg == '!resume') {
             var track = sinusbot.getCurrentTrack();
-            sinusbot.getCurrentChannel().chat('test');
             if (!track) return;
-            var pos = store.get(track.ID());
+            var pos = sinusbot.getVar(track.uuid);
             if (!pos) {
-                sinusbot.getCurrentChannel().chat('No position found, sorry.');
+                sinusbot.chatChannel('No position found, sorry.');
                 return;
             }
-            track.play();
-            audio.seek(pos);
-            sinusbot.getCurrentChannel().chat('Resumed at ' + pos + 'ms.');
-        //if (ev.msg == '.resume') {
-        //    var track = sinusbot.getCurrentTrack();
-        //    if (!track) return;
-        //    var pos = sinusbot.getVar(track.uuid);
-        //    if (!pos) {
-        //        sinusbot.chatChannel('No position found, sorry.');
-        //        return;
-        //    }
-        //    sinusbot.seek(pos);
-        //    sinusbot.chatChannel('Resumed at ' + pos + 'ms.');
-        //}
+            sinusbot.play();
+            sinusbot.seek(pos);
+            sinusbot.chatChannel('Resumed at ' + pos + 'ms.');
+            sinusbot.setRepeat(false);
+            //sinusbot.setMute(false);
+        }
     });
 });
