8ba8f1c1a64624de1230ddc0528e821e3af829b1
Shawye <Shawye@MSI>
2017-10-07 18:37:50 -0700

    Begins creation of playlist associated commands

--- S2/js/main.js
+++ S2/js/main.js
@@ -10,30 +10,30 @@ registerPlugin({
         var chatMessage = ev.msg;
         var args = chatMessage.split(" ");
 
-        if (ev.mode < 3 && args.length >= 1 && args[0].toLowerCase() == "!roll") { //If it's a valid !roll command
-            var max = 100;
-            var message = "";
+        //if (ev.mode < 3 && args.length >= 1 && args[0].toLowerCase() == "!roll") { //If it's a valid !roll command
+        //    var max = 100;
+        //    var message = "";
 
-            if (args.length == 2) {
-                if (!isNaN(args[1])) {
-                    max = parseInt(args[1]);
-                }
-            }
+        //    if (args.length == 2) {
+        //        if (!isNaN(args[1])) {
+        //            max = parseInt(args[1]);
+        //        }
+        //    }
 
-            if (args.length <= 2) {
-                message = "[COLOR=#aa00ff]" + ev.clientNick + "[/COLOR][COLOR=#005500] rolled: [/COLOR][COLOR=#ff5500]" + generateRandomInteger(max).toString() + "[/COLOR]";
-            } else {
-                message = "[COLOR=#55ffff]Usage: [/COLOR][COLOR=#ffaa00]!roll <max>[/COLOR]";
-            }
+        //    if (args.length <= 2) {
+        //        message = "[COLOR=#aa00ff]" + ev.clientNick + "[/COLOR][COLOR=#005500] rolled: [/COLOR][COLOR=#ff5500]" + generateRandomInteger(max).toString() + "[/COLOR]";
+        //    } else {
+        //        message = "[COLOR=#55ffff]Usage: [/COLOR][COLOR=#ffaa00]!roll <max>[/COLOR]";
+        //    }
 
-            if (ev.mode == 2) {
-                chatChannel(message);
-            } else {
-                chatPrivate(ev.clientId, message);
-            }
+        //    if (ev.mode == 2) {
+        //        chatChannel(message);
+        //    } else {
+        //        chatPrivate(ev.clientId, message);
+        //    }
         }
     });
-    // strange
+
     function generateRandomInteger(max) {
         return Math.round(Math.random() * max);
     }
