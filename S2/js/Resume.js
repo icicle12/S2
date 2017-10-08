e533b69f6282d504e99a0651b44af83e78175cc4
icicle12 <mistcomme@gmail.com>
2017-10-07 19:40:49 -0700

    Update Resume.js

--- S2/js/Resume.js
+++ S2/js/Resume.js
@@ -1,1 +1,43 @@
+//Some code taken from 'Bookmarks!' Script
+registerPlugin({
+    name: 'Resume',
+    version: '1.0',
+    description: 'Enter !pause to pause the song, enter !resume to seek to unpause the song.',
+    author: 'Shawye <shawye@ucla.ed>','Bert'
+    vars: []
+}, function (sinusbot, config) {
 
+    var store = require('store'),
+        media = require('media'),
+        backend = require('backend'),
+        audio = require('audio');
+
+    var event = require('event');
+    event.on('chat', function (ev) {
+        ev.client.chat('Hi, ' + ev.client.name() + ', you just wrote: ' + ev.text);
+    });
+
+    event.on('chat', function (ev) {
+        engine.log(ev.text);
+        if (ev.text == '.bookmark') {
+            engine.log("bookmark");
+            var track = media.getCurrentTrack();
+            if (!track) return;
+            var pos = audio.getTrackPosition();
+            store.set(track.ID(), pos);
+            backend.getCurrentChannel().chat('Position saved for track ' + track.uuid + ' at ' + pos + 'ms.');
+        }
+        if (ev.text == '.resume') {
+            engine.log("resume");
+            var track = media.getCurrentTrack();
+            if (!track) return;
+            var pos = store.get(track.ID());
+            if (!pos) {
+                backend.getCurrentChannel().chat('No position found, sorry.');
+                return;
+            }
+            audio.seek(pos);
+            backend.getCurrentChannel().chat('Resumed at ' + pos + 'ms.');
+        }
+    });
+});
