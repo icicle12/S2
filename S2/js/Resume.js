860d81fc0b1014afd83f8b0a46f22ea6d14e4628
Roberto <30516241+lyceris@users.noreply.github.com>
2017-10-08 04:33:13 -0400

    Update Resume.js

--- S2/js/Resume.js
+++ S2/js/Resume.js
@@ -28,7 +28,7 @@ registerPlugin({
             var minutes = Math.floor(pos / 60000),
                 seconds = ((pos % 60000) / 1000).toFixed(0),
                 time = minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
-            backend.getCurrentChannel().chat('Position saved for track ' + track.title + ' at ' + time + '.');
+            backend.getCurrentChannel().chat('Position saved for track ' + track.title + ' at ' + string(time) + '.');
             media.stop();
         }
         
@@ -46,7 +46,7 @@ registerPlugin({
                 time = minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
             track.play();
             audio.seek(pos);
-            backend.getCurrentChannel().chat('Resumed' + track.title + ' at ' + time + '.');
+            backend.getCurrentChannel().chat('Resumed' + track.title + ' at ' + string(time) + '.');
         }
     });
 });
